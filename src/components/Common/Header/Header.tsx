2cc54ccfaabf98d93552c674fd69704b04ce01db
Dylan5023 <kangjun5023@gmail.com>
2023-09-23 17:32:13 -0700

    create register modal

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,9 +1,22 @@
+"use client";
+import RegisterKeywordModal from "@/components/RegisterKeywordModal";
+import { useState } from "react";
+
 function Header() {
+  const [openKeywordModal, setOpenKeywordModal] = useState(false);
   return (
-    <div className="fixed h-[60px] z-10 bg-black text-white w-[420px] flex items-center justify-between px-4">
-      <div className="text-xl font-semibold">My article</div>
-      <button className="text-xl">Register</button>
-    </div>
+    <>
+      <div className="fixed h-[60px] z-10 bg-black text-white w-[420px] flex items-center justify-between px-4">
+        <div className="text-xl font-semibold">My Article</div>
+        <button className="text-xl" onClick={() => setOpenKeywordModal(true)}>
+          Register
+        </button>
+      </div>
+      <RegisterKeywordModal
+        isOpen={openKeywordModal}
+        onOpenClose={() => setOpenKeywordModal(false)}
+      />
+    </>
   );
 }
 
---
+++ src/components/RegisterKeywordModal/RegisterKeywordModal.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { registerKeyword } from "@/app/api/keyword/registerKeyword";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Checkbox,
+  Input,
+  Link,
+} from "@nextui-org/react";
+import { useState } from "react";
+
+// create interface
+interface RegisterKeywordModalProps {
+  isOpen: boolean;
+  onOpenClose: () => void;
+}
+
+// create function of modal content
+function RegisterKeywordModal({
+  isOpen,
+  onOpenClose,
+}: RegisterKeywordModalProps) {
+  const [keyword, setKeyword] = useState("");
+
+  const handleSubmit = async () => {
+    if (keyword !== "") {
+      await registerKeyword({ keyword });
+      alert("Registed.");
+      onOpenClose();
+    } else {
+      alert("Put the keyword");
+    }
+  };
+
+  return (
+    <Modal size="xs" isOpen={isOpen} onOpenChange={onOpenClose}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader>Register Keyword</ModalHeader>
+            <ModalBody>
+              <Input
+                autoFocus
+                placeholder="Put the Keyword"
+                variant="bordered"
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="flat" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button color="primary" onPress={handleSubmit}>
+                Register
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default RegisterKeywordModal;
